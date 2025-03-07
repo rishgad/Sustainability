26bfd674dd8b789978ec49ef9eeb277a91c306ba
rishgad <161489567+rishgad@users.noreply.github.com>
2025-03-06 19:26:43 -0600

    Create Adding.jsx

---
+++ sustainability/src/screens/Adding.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import "./style.css";
+
+export const Adding = () => {
+  return (
+    <div className="adding">
+      <div className="set-up-your-home">
+        Set up your <br />
+        Home!
+      </div>
+
+      <div className="overlap-group">
+        <img
+          className="vector"
+          alt="Vector"
+          src="https://c.animaapp.com/Trn1a7BP/img/vector.svg"
+        />
+      </div>
+
+      <div className="overlap">
+        <div className="rectangle" />
+
+        <div className="room-name">&lt;Room Name&gt;</div>
+
+        <div className="appliance-name">&lt;Appliance Name&gt;</div>
+
+        <div className="text-wrapper">&lt;Appliance Name&gt;</div>
+
+        <div className="div">&lt;Appliance Name&gt;</div>
+      </div>
+
+      <div className="overlap-2">
+        <div className="rectangle-2" />
+
+        <div className="appliance-name-2">&lt;Appliance Name&gt;</div>
+
+        <div className="appliance-name-3">&lt;Appliance Name&gt;</div>
+
+        <div className="room-name-2">&lt;Room Name&gt;</div>
+
+        <div className="rectangle-3" />
+
+        <div className="text-wrapper-2">Continue</div>
+
+        <img
+          className="img"
+          alt="Rectangle"
+          src="https://c.animaapp.com/Trn1a7BP/img/rectangle-29.svg"
+        />
+
+        <img
+          className="rectangle-4"
+          alt="Rectangle"
+          src="https://c.animaapp.com/Trn1a7BP/img/rectangle-22.svg"
+        />
+
+        <img
+          className="rectangle-5"
+          alt="Rectangle"
+          src="https://c.animaapp.com/Trn1a7BP/img/rectangle-30.svg"
+        />
+
+        <img
+          className="rectangle-6"
+          alt="Rectangle"
+          src="https://c.animaapp.com/Trn1a7BP/img/rectangle-31.svg"
+        />
+
+        <div className="text-wrapper-3">Room</div>
+
+        <div className="text-wrapper-4">Done</div>
+
+        <div className="text-wrapper-5">Appliance</div>
+
+        <div className="rectangle-7" />
+
+        <div className="text-wrapper-6">Room Name</div>
+      </div>
+    </div>
+  );
+};
