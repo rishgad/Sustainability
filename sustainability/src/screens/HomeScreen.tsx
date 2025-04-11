0f304f9fb4a8b9eecfaa1d500c64a62468c3b89b
marie cho <mariecho@wireless-10-150-52-9.public.utexas.edu>
2025-04-10 19:56:40 -0500

    start scratch

--- sustainability/src/screens/HomeScreen.tsx
+++ sustainability/src/screens/HomeScreen.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import { Text } from 'react-native'
 import { View, Image, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+
+
 const HomeScreen = () => {
-  const navigation = useNavigation();
-  const handleRoomClick = (room: string) => {
-    navigation.navigate(room);
-  };
-  return (
-    <View>
-      <TouchableOpacity onPress={() => handleRoomClick('Kitchen')}>
-        <Image source={require('../assets/houseImage.png')} />
-      </TouchableOpacity>
-    </View>
-  );
-};
+    const navigation = useNavigation(); 
+
+    const handleRoomClick = (room: string) => { 
+        navigation.navigate(room);
+    };
+
+    return (
+        <View>
+            <Text>I am the homescreen!</Text>
+            <TouchableOpacity onPress={() => handleRoomClick('KitchenScreen')}>
+                <Image source={require('../assets/favicon.png')} />
+            </TouchableOpacity>
+        </View>
+        
+    )
+
+
+
+}
+
+
+
 export default HomeScreen;
